edd4a79f74e5100be0638c67aa9b4691f4389a94
Yu Han Lin <ylin9720@gmail.com>
2016-10-02 00:02:55 -0400

    File Clean up

--- Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver
-{
-    public static void main(String[] args)
-    {
-        MenuGUI a = new MenuGUI();//Creates a MenuGUI object that will help user navigate throughout program, is the starting JFrame of all JDialogs
-        a.showMenu();//Sets the JFrame to visible and shows the Menu 
-    }
-}
